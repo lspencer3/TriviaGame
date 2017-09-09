9b89fc194f7289e85610d6ce23d7455b91e7fd9d
Lauren <lauren.spencer007@gmail.com>
2017-09-09 09:14:11 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,61 +23,93 @@ var disneyQuiz = [questions.q1, questions.q2, questions.q3, questions.q4, questi
 var disneyQuiza = [questions.a1, questions.a2, questions.a3, questions.a4, questions.a5];
 var disneyAnswers = ["apple","true", "a pumpkin", "Rajah", "gorrillas"];
 
-//set timeout function that goes to next question and answers after 8 secs, also pushes answers to new array
-function eightSeconds() {
+//setup click functionality to assign data attr to submit button when clicked
 
-//***when submit button is pressed push checked form to userAnswers array else push undefined
-$("#submit").click(function(){
-//***should run by length
-	if (index > disneyQuiz.length-1){
-		return
-		}
-
-else{
-		userAnswers.push($('input[name="answer"]:checked').val());
-	}
-
+$('#submit').click(function(){
+  $(this).data('clicked', true);
 });
 
-  alert("times Up!");
- 
-  //increment index var
-  index++
+//set timeout function that throws an alert pushes undefined to userAnsers array a changes to next question and answers after 8 secs
+function eightSeconds() {
 
-  //rerun game functions
-  displayQuestion()
-  displayAnswers()
-}
+	alert("Times Up! Next Question");
+	userAnswers.push(undefined);
+
+    //increment index var
+    index++
+
+	//rerun game functions
+ 	displayQuestion()
+  	displayAnswers()
+	
+};
 
 //create function to show questions for 8 seconds
 function displayQuestion(){
 
-	if (index < disneyQuiz.length-1) {
-
-   		$("#question").html(disneyQuiz[index]);
-
-      }
-//add score together at index = 5
-	else {
+//stop at 5 add score together at index = 5
+	if (index =disneyQuiz.length) {
 			for (i=0; i<userAnswers.length; i++){
 					if (userAnswers[i] === disneyAnswers[i]){
 							score= score + 1
 							}
 					}
-	//$("#result").html("You Scored" + score + "/" + index < disneyQuiz.length + "!")
+	$("#result").html("You Scored" + score + "/" + disneyQuiz.length + "!") 
+	console.log(score)
 			}
+
+
+else if (index <= disneyQuiz.length-1) {
+
+   		$("#question").html(disneyQuiz[index]);
+
+      }
 };
+
 //create for loop to loop through answers arrays and create answer forms
 function displayAnswers(){
-	$("#answers").empty()
+	
+	if(index >=disneyQuiza.length){
+		console.log(score)
+	}
+
+	else if (index <=disneyQuiza.length-1){
+		$("#answers").empty()
+
 	for (i = 0; i < disneyQuiza[index].length; i++){ 
 		
 		var a = $('<label><input type = "radio" id = "answers" name = "answer" value="' + disneyQuiza[index][i] + '" /> ' + disneyQuiza[index][i] + '</label><br>')
 
 				$("#answers").append(a)
 						}
-setTimeout(eightSeconds, 1000 * 8);
-			};
+				
+
+//set time after answers displayed
+var time = setTimeout(eightSeconds, 1000 * 8);
+   
+
+//when submit button is pressed push checked form to userAnswers array
+if ($("#submit").data('clicked')){
+
+	//clear timeout function
+    function timerStop() {
+    clearTimeout(time);
+			}
+    //clear timeout
+	timerStop()
+
+	userAnswers.push($('input[name="answer"]:checked').val());
+	
+	//increment index var
+    index++
+
+	//rerun game functions
+ 	displayQuestion()
+  	displayAnswers()
+         }
+	}
+
+};
 
 
 
