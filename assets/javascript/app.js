5dc828e199c830a3bb4521ea064bc057e488c8fb
Lauren <lauren.spencer007@gmail.com>
2017-09-10 17:25:45 -0400

    working game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,8 @@
 //create needed variables
 var score = 0;
 var index = 0;
-var userAnswers =[];
+var userAnswers = [];
+var time = 0;
 
 // create object questions for the Trivia game.
 var questions = {
@@ -24,9 +25,9 @@ var disneyQuiza = [questions.a1, questions.a2, questions.a3, questions.a4, quest
 var disneyAnswers = ["apple","true", "a pumpkin", "Rajah", "gorrillas"];
 
 //setup click functionality to assign data attr to submit button when clicked
-
 $('#submit').click(function(){
-  $(this).data('clicked', true);
+  
+	$(this).data('clicked', true);
 });
 
 //set timeout function that throws an alert pushes undefined to userAnsers array a changes to next question and answers after 8 secs
@@ -40,63 +41,60 @@ function eightSeconds() {
 
 	//rerun game functions
  	displayQuestion()
-  	displayAnswers()
-	
-};
+  	displayAnswers()	
+}
 
-//create function to show questions for 8 seconds
-function displayQuestion(){
+//create function to show questions 
+function displayQuestion() {
 
-//stop at 5 add score together at index = 5
-	if (index =disneyQuiz.length) {
-			for (i=0; i<userAnswers.length; i++){
-					if (userAnswers[i] === disneyAnswers[i]){
-							score= score + 1
-							}
-					}
-	$("#result").html("You Scored" + score + "/" + disneyQuiz.length + "!") 
-	console.log(score)
+	//stop at 5 add score together at index = 5
+	if (index ===disneyQuiz.length) {
+		for (i=0; i<userAnswers.length; i++) {
+			if (userAnswers[i] === disneyAnswers[i]) {
+				score = score + 1
 			}
+		}
 
-
-else if (index <= disneyQuiz.length-1) {
-
-   		$("#question").html(disneyQuiz[index]);
-
-      }
-};
-
-//create for loop to loop through answers arrays and create answer forms
-function displayAnswers(){
-	
-	if(index >=disneyQuiza.length){
+		$("#result").html("You Scored" + score + "/" + disneyQuiz.length + "!") 
 		console.log(score)
 	}
 
-	else if (index <=disneyQuiza.length-1){
+
+	else if (index < disneyQuiz.length) {
+
+   		$("#question").html(disneyQuiz[index]);
+    }
+};
+
+//create for loop to loop through answers arrays and create answer forms to show for 8 seconds
+function displayAnswers() {
+
+	if(index === disneyQuiza.length) {
+		
+		console.log(score)
+	}
+
+	else if (index < disneyQuiza.length) {
+		
 		$("#answers").empty()
 
-	for (i = 0; i < disneyQuiza[index].length; i++){ 
+		for (i = 0; i < disneyQuiza[index].length; i++) { 
 		
-		var a = $('<label><input type = "radio" id = "answers" name = "answer" value="' + disneyQuiza[index][i] + '" /> ' + disneyQuiza[index][i] + '</label><br>')
+			var a = $('<label><input type = "radio" id = "answers" name = "answer" value="' + disneyQuiza[index][i] + '" /> ' + disneyQuiza[index][i] + '</label><br>')
 
-				$("#answers").append(a)
-						}
-				
-
-//set time after answers displayed
-var time = setTimeout(eightSeconds, 1000 * 8);
-   
+			$("#answers").append(a)
+		}			
+		
+		//set time after answers displayed
+		time = setTimeout(eightSeconds, 1000 * 8);   
+	}
+};
 
 //when submit button is pressed push checked form to userAnswers array
-if ($("#submit").data('clicked')){
-
-	//clear timeout function
-    function timerStop() {
-    clearTimeout(time);
-			}
-    //clear timeout
-	timerStop()
+$("#submit").click(function() {
+    
+    //clear timeout	
+	clearTimeout(time);
 
 	userAnswers.push($('input[name="answer"]:checked').val());
 	
@@ -106,12 +104,7 @@ if ($("#submit").data('clicked')){
 	//rerun game functions
  	displayQuestion()
   	displayAnswers()
-         }
-	}
-
-};
-
-
+});
 
 //start game by calling displayQuestion and displayAnswers functions
 displayQuestion();
